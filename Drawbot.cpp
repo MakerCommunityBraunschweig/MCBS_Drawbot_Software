012d2b8eee856fe577d03534fd07986d49f45ccd
MartinDml <dehmel-martin@web.de>
2019-01-09 23:53:22 +0100

    Test1

--- Drawbot.cpp
+++ Drawbot.cpp
@@ -132,12 +132,17 @@ void Drawbot::set_path(int x[], int y[]) {
 
 void Drawbot::move_path(int m1_pos[], int m2_pos[]) {
 
+<<<<<<< HEAD
   int nop = 6;
   for (int i=0; i < 3; i++) {
+=======
+  int nop = sizeof(m1_pos);
+  for (int i=0; i <= nop; i++) {
+>>>>>>> parent of c002de1... Grenzüberwachung adaptiert
     set_target_values(m1_pos[i],m2_pos[i]);
     Serial.println("Target: " + String(m1_pos[i]) + " | " + String(m2_pos[i]));
     move_to_target();
-    delay(50); 
+    delay(2000); 
   }
       
 }
@@ -248,13 +253,13 @@ bool Drawbot::check_boundaries(int m1, int m2) {
 
   bool valid = true;
 
-  if ((m1 < m1_max + 100) and (m1 > m1_min - 100)) {
+  if ((m1 < m1_max) and (m1 > m1_min)) {
     // do nothing
   } else {
     Serial.println("Desired position of motor 1 invalid!");
     valid = false;
   }
-  if ((m2 < m2_max + 100) and (m2 > m2_min - 100)) {
+  if ((m2 < m2_max) and (m2 > m2_min)) {
     // do nothing
   } else {
     Serial.println("Desired position of motor 2 invalid!");
--- Drawbot_Software.ino
+++ Drawbot_Software.ino
@@ -45,7 +45,6 @@ void setup() {
 
   
   db.set_delayUs(1000);
-  db.isHomed = false;
   db.set_directions(FWD,FWD);
   db.home_all();
 
@@ -66,17 +65,7 @@ void loop () {
   db.set_directions(FWD,FWD);
   db.move_linear_in_js(1700,1000);
   
-
   db.home_all();
- //db.manual_mode();
-  
-  db.set_delayUs(300);                   // Defines the velocity
-  
-//Move this path 5 times
- 
-//  for(int i = 1; i <= 5; i++) {
-//    db.move_path(m1_pos, m2_pos);
-//  }
 
   delay(10000);
 }
